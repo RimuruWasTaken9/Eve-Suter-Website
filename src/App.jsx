6b537f16ab94cddcdc3bad00ed6f405f647de806
Jordan Fable <jordan.fable711@gmail.com>
2025-01-18 23:27:17 -0500

    template eve suter website

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import imageGroups from "./assets/imageURLs.js";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>MY WEBSITE</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Does this work?
-      </p>
-    </>
-  )
+    <div className="gallery-container">
+      <h1 className="main-title">Eve Suter's Portfolio</h1>
+      {imageGroups.map((group, groupIndex) => (
+        <div key={groupIndex} className="group-container">
+          <h2 className="group-title">{group.title}</h2>
+          <div className="image-row">
+            {group.images.map((url, index) => (
+              <img
+                key={index}
+                src={url}
+                alt={`Artwork ${groupIndex + 1}-${index + 1}`}
+                className="image"
+              />
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/assets/imageURLs.js
@@ -0,0 +1,41 @@
+
+const imageGroups = [
+{
+    title: "Fall",
+    images: [
+"https://res.cloudinary.com/dpbsgzrho/image/upload/v1737241796/fall4_pnz2nh.jpg",
+"https://res.cloudinary.com/dpbsgzrho/image/upload/v1737241796/fall2_uor82w.jpg",
+"https://res.cloudinary.com/dpbsgzrho/image/upload/v1737241796/fall3_ojzibh.jpg",
+"https://res.cloudinary.com/dpbsgzrho/image/upload/v1737241796/fall1_jovooh.jpg",
+    ],
+},
+{
+    title: "Winter",
+    images: [
+"https://res.cloudinary.com/dpbsgzrho/image/upload/v1737241786/winter4_y8phyy.jpg",
+"https://res.cloudinary.com/dpbsgzrho/image/upload/v1737241786/winter1_asj5sp.jpg",
+"https://res.cloudinary.com/dpbsgzrho/image/upload/v1737241786/winter3_bukl2z.jpg",
+"https://res.cloudinary.com/dpbsgzrho/image/upload/v1737241786/winter2_ptkmqj.jpg",
+    ],
+},
+{
+    title: "Summer",
+    images: [
+"https://res.cloudinary.com/dpbsgzrho/image/upload/v1737241785/summer3_rj4xxx.jpg",
+"https://res.cloudinary.com/dpbsgzrho/image/upload/v1737241785/summer2_bxlpyu.jpg",
+"https://res.cloudinary.com/dpbsgzrho/image/upload/v1737241785/summer1_bnlmcb.jpg",
+"https://res.cloudinary.com/dpbsgzrho/image/upload/v1737241785/summer4_dougzf.jpg",
+    ],
+},
+{
+    title: "Spring",
+    images: [
+"https://res.cloudinary.com/dpbsgzrho/image/upload/v1737241785/spring1_zngksm.jpg",
+"https://res.cloudinary.com/dpbsgzrho/image/upload/v1737241785/spring4_qnxw6d.jpg",
+"https://res.cloudinary.com/dpbsgzrho/image/upload/v1737241785/spring2_p9k6wz.jpg",
+"https://res.cloudinary.com/dpbsgzrho/image/upload/v1737241785/spring3_aihng7.jpg"
+    ],
+},
+];
+
+export default imageGroups;
