73f76fe37686fcf3a1146c35b145838e5e79ac6e
Jordan Fable <jordan.fable711@gmail.com>
2025-02-02 16:08:21 -0500

    test deploy 6

---
+++ src/components/CreateSeason.jsx
@@ -0,0 +1,47 @@
+import Autoplay from "embla-carousel-autoplay";
+import { Link } from "react-router-dom";
+
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+    CarouselDots,
+} from "@/components/ui/carousel";
+import { scale } from "@cloudinary/url-gen/actions/resize";
+
+
+function CreateSeason(props) {
+    const images = [props.img1, props.img2, props.img3, props.img4];
+
+
+    return (
+        <div className="flex flex-col m-3">
+            
+            <Link to="/" className="pl-0.5 pt-5 pb-3">
+                <img src={props.logo} />
+            </Link>
+            
+                
+                <Carousel
+                    opts={{
+                        align: "start",
+                        loop: true,
+                    }}
+                >
+                    <CarouselContent>
+                        <CarouselItem><img src={props.img1} /></CarouselItem>
+                        <CarouselItem><img src={props.img2} /></CarouselItem>
+                        <CarouselItem><img src={props.img3} /></CarouselItem>
+                        <CarouselItem><img src={props.img4} /></CarouselItem>
+                    </CarouselContent>
+                   
+                    <CarouselDots className="p-4"/>
+                </Carousel>
+        </div>
+
+    );
+}
+
+export default CreateSeason;
